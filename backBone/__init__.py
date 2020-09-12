ab2ddf5ae6898024b0bc003f48befd0c711b0e39
Alice <ghj9527@163.com>
2020-09-12 11:20:58 +0800

    v1

---
+++ backBone/__init__.py
@@ -0,0 +1,5 @@
+from backBone.densenet import DenseNet
+from backBone.resnet import ResNet
+from backBone.vgg import VGG
+from backBone.xception import Xception
+from backBone.mobilenet import MobileNet
